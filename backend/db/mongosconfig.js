d1a1b66e6bb075d284437d67b6e1861bf95eb025
naveedkapoorkhan <147315324+naveedkapoorkhan@users.noreply.github.com>
2026-01-30 21:55:15 +0500

    Update MongoDB connection string for KisanMandi

--- backend/db/mongosconfig.js
+++ backend/db/mongosconfig.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 mongoose
   .connect(
-    "mongodb+srv://naveedkapoorkhan:1wwgDAJUCLCegm8n@cluster0.sccr1tc.mongodb.net/Farmer?retryWrites=true&w=majority",
+    "mongodb+srv://naveedkapoorkhan_db_user:qYvZvHUPOFKYHJ1b@product-shop.g3w9pzp.mongodb.net/KisanMandi",
+    //"mongodb+srv://naveedkapoorkhan:1wwgDAJUCLCegm8n@cluster0.sccr1tc.mongodb.net/Farmer?retryWrites=true&w=majority",
     { useNewUrlParser: true, useUnifiedTopology: true, } //yeah object pura isi tarha se ay ga bydefault
   )
   .then(() => {
